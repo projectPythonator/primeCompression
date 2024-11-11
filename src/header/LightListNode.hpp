3a10883898d5ec418c9e0f5d8aff88f14bb98e33
projectPythonator <danjkstraProtonAlt1@protonmail.com>
2024-11-10 20:13:59 -0500

    ADDED added comment line bcause unsure if works

--- src/header/LightListNode.hpp
+++ src/header/LightListNode.hpp
@@ -4,12 +4,12 @@
 #include <cinttypes>
 
 namespace pseudoConstNode {
-
+// do I need to add an initializer func or turn this into 
 struct Node {
-    Node *next = nullptr;
+    Node *nextNode = nullptr; 
     std::uint32_t primeIndex = 0;
     void setNext(Node *newNext) {
-        next = newNext;
+        nextNode = newNext;
     }
 };
 
