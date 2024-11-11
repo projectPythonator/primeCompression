232c989e501027e93455913a86d763240ff32409
projectPythonator <danjkstraProtonAlt1@protonmail.com>
2024-11-10 20:08:14 -0500

    CHNAGED removed the Node struct                   it got moved to its own file also changed the two functions i had going on I decided we will just have push and pop the head worst comes to worst I will change the code rn the class works by passing nodes further down the structure we have still yet to get to the data structure above this one

--- src/header/LightLinkedList.hpp
+++ src/header/LightLinkedList.hpp
@@ -1,20 +1,16 @@
 #ifndef LIGHTLINKEDLIST_HPP
 #define LIGHTLINKEDLIST_HPP
 
+#include "LightListNode.hpp"
 #include <mutex>
 
 namespace protectedLinkedList {
 
-struct Node {
-    Node *next;
-    std::uint32_t primeIndex;
-};
-
 class LinkedList {
 public:
     LinkedList();
-    void addNode(Node *);
-    void resetHead();
+    void pushHead(Node *);
+    Node *popHead();
 
     constexpr std::size_t size() {
         return N;
