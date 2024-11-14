c1e21ef3e18120b1dda704af06b5a82223e5559a
projectPythonator <danjkstraProtonAlt1@protonmail.com>
2024-11-14 14:01:47 -0500

    CHANGED wip file contains a fast mod now Chnaged using fast mod method which is same idea but then using the result in n - result * 30 for some reason this requires the + 1 version of the 2^n/mod >> n as all values n %30 == 0 return 30, on my totally lose test of speed in a totally not decisive benchmark uint64_t i % 30 upto 2^32 was 2.7s and using my method was 2.3s :) so might consider using this especially if it is consistently this speed as when I tried testing speed on division the code around i%30 affected the optimization a lot more than my custom "fastdiv"

--- src/WIP_code_place_holder.cc
+++ src/WIP_code_place_holder.cc
@@ -1,6 +1,7 @@
 // fast division for inner loops will need to check if pre computing is still fsater for the smallest of not presieve primes
 // the two values represent floor(2**68/30) and 68 is to normalize it again
-size_t fastdiv(uint64_t n) {
-    return ((__uint128_t (n) * 9838263505978427528ULL) >> 68) % BUF_SIZE;
+// changed to be fast mod 30 
+size_t fastmod(uint64_t n) {
+    return n - ((__uint128_t (n) * 9838263505978427529ULL) >> 68) * 30;
 }
 
