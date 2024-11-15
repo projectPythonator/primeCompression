034cd88b2e5af341f5d9c415aaa6bb4ca7d91577
projectPythonator <danjkstraProtonAlt1@protonmail.com>
2024-11-15 07:13:55 -0500

    ADDED file as place holder for memory pooling for now this file is simply a place holder I might not even need it in its entirty but having it there as a reminder

---
+++ src/header/MemoryPool.hpp
@@ -0,0 +1,7 @@
+// adding this file here as a place holder
+// right now I am not sure if I want to build my own or use an existing one
+// the idea is I will pass nodes arround in my data structrue for big primes
+// this way I never have to alloc or free memory in vectors or have static
+// amounts in arrays it might be a bit slower but will need to test it out
+// will try to make the data strcuture class such that I can place either or
+// in to test speed and stuff
