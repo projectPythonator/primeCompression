5f1a92238d68ac1ac13dc3e587106b83d59b239e
projectPythonator <danjkstraProtonAlt1@protonmail.com>
2024-11-16 13:12:19 -0500

    CHANGE added bulk switch code and changed names changed currentIndex to byteIndex I think it works better changed define in that I fixed a bug with missing semicolon added bulk switch statement code with following logic checkBound(the define with goto) -> apply mask at byteindex -> increment byte index -> fallthrough NOTE: i used [[fallthrough]] I have seen a handful of people use FALLTHROUGH; then it being defined somewhere else but I don't have reason to yet. I will get around to that sometime, NOTE2: I might add a second file or might not for basically skipping the iteration style step and storing it in a lookup array but that is pending for now I will stick to keeping it simple previously I tried etching out max amount with lookups but now I understand what [6, 4, 2, 4, 2, 4, 6, 2] means from wikipedia so I can utilize it in my code NOTE3: will need to see if I can remove magic numbers with constexpr or something NOTE4: medium / faster with check primes will use similar statements but without the case 0 loop bodies I think will need to rerun my python info script to see the bounds

--- src/FastPrimesWithNoCheck.cpp
+++ src/FastPrimesWithNoCheck.cpp
@@ -16,128 +16,129 @@ void crossOff(
         std::size_t sieveSize, 
         std::size_t startIndex, 
         std::size_t endIndex) {
-    #define IS_DONE_SIEVING(maskIndex) \
-        if (kValue >= sieveSize) { \
-            updatePrimeInfo(primeIndex, currentIndex, maskIndex) \
+    #define IS_DONE(maskIndex) \
+        if (kValue >= sieveSize) \
+        { \
+            updatePrimeInfo(primeIndex, byteIndex, maskIndex); \
             goto next_prime; \
-        } \
+        }
 
     
     for (std::size_t primeIndex = startIndex; primeIndex < endIndex; primeIndex++) {
-        std::size_t currentIndex;   // represents the current global index of this prime
+        std::size_t byteIndex;   // represents the current global index of this prime
         std::size_t kValue;         // place holder for 30 * k + b for k
-        std::size_t maskIndex;      // modified version of (currentIndex % 30) 
+        std::size_t maskIndex;      // modified version of (byteIndex % 30) 
         switch (maskIndex) {
             // handles primes of form 30 * k + 1
             for (;;) {
                 case 0: {
-                            continue;
+                            IS_DONE(0); sieve[byteIndex] &= MASK0; byteIndex += kValue * 6 + 0; [[fallthrough]];
                         }
-                case 1: continue;
-                case 2: continue;
-                case 3: continue;
-                case 4: continue;
-                case 5: continue;
-                case 6: continue;
-                case 7: continue;
+                case 1: IS_DONE(1); sieve[byteIndex] &= MASK0; byteIndex += kValue * 4 + 0; [[fallthrough]];
+                case 2: IS_DONE(2); sieve[byteIndex] &= MASK0; byteIndex += kValue * 2 + 0; [[fallthrough]];
+                case 3: IS_DONE(3); sieve[byteIndex] &= MASK0; byteIndex += kValue * 4 + 0; [[fallthrough]];
+                case 4: IS_DONE(4); sieve[byteIndex] &= MASK0; byteIndex += kValue * 2 + 0; [[fallthrough]];
+                case 5: IS_DONE(5); sieve[byteIndex] &= MASK0; byteIndex += kValue * 4 + 0; [[fallthrough]];
+                case 6: IS_DONE(6); sieve[byteIndex] &= MASK0; byteIndex += kValue * 6 + 0; [[fallthrough]];
+                case 7: IS_DONE(7); sieve[byteIndex] &= MASK0; byteIndex += kValue * 2 + 1; [[fallthrough]];
             }
 
             // handles primes of form 30 * k + 7
             for (;;) {
                 case  8: {
-                            continue;
+                            IS_DONE( 8); sieve[byteIndex] &= MASK0; byteIndex += kValue * 6 + 1; [[fallthrough]];
                          }
-                case  9: continue;
-                case 10: continue;
-                case 11: continue;
-                case 12: continue;
-                case 13: continue;
-                case 14: continue;
-                case 15: continue;
+                case  9: IS_DONE( 9); sieve[byteIndex] &= MASK0; byteIndex += kValue * 4 + 1; [[fallthrough]];
+                case 10: IS_DONE(10); sieve[byteIndex] &= MASK0; byteIndex += kValue * 2 + 1; [[fallthrough]];
+                case 11: IS_DONE(11); sieve[byteIndex] &= MASK0; byteIndex += kValue * 4 + 0; [[fallthrough]];
+                case 12: IS_DONE(12); sieve[byteIndex] &= MASK0; byteIndex += kValue * 2 + 1; [[fallthrough]];
+                case 13: IS_DONE(13); sieve[byteIndex] &= MASK0; byteIndex += kValue * 4 + 1; [[fallthrough]];
+                case 14: IS_DONE(14); sieve[byteIndex] &= MASK0; byteIndex += kValue * 6 + 1; [[fallthrough]];
+                case 15: IS_DONE(15); sieve[byteIndex] &= MASK0; byteIndex += kValue * 2 + 1; [[fallthrough]];
             }
 
             // handles primes of form 30 * k + 11
             for (;;) {
                 case 16: {
-                            continue;
+                            IS_DONE(16); sieve[byteIndex] &= MASK0; byteIndex += kValue * 6 + 2; [[fallthrough]];
                         }
-                case 17: continue;
-                case 18: continue;
-                case 19: continue;
-                case 20: continue;
-                case 21: continue;
-                case 22: continue;
-                case 23: continue;
+                case 17: IS_DONE(17); sieve[byteIndex] &= MASK0; byteIndex += kValue * 4 + 2; [[fallthrough]];
+                case 18: IS_DONE(18); sieve[byteIndex] &= MASK0; byteIndex += kValue * 2 + 0; [[fallthrough]];
+                case 19: IS_DONE(19); sieve[byteIndex] &= MASK0; byteIndex += kValue * 4 + 2; [[fallthrough]];
+                case 20: IS_DONE(20); sieve[byteIndex] &= MASK0; byteIndex += kValue * 2 + 0; [[fallthrough]];
+                case 21: IS_DONE(21); sieve[byteIndex] &= MASK0; byteIndex += kValue * 4 + 2; [[fallthrough]];
+                case 22: IS_DONE(22); sieve[byteIndex] &= MASK0; byteIndex += kValue * 6 + 2; [[fallthrough]];
+                case 23: IS_DONE(23); sieve[byteIndex] &= MASK0; byteIndex += kValue * 2 + 1; [[fallthrough]];
             }
 
             // handles primes of form 30 * k + 13
             for (;;) {
                 case 24: {
-                            continue;
+                            IS_DONE(24); sieve[byteIndex] &= MASK0; byteIndex += kValue * 6 + 3; [[fallthrough]];
                         }
-                case 25: continue;
-                case 26: continue;
-                case 27: continue;
-                case 28: continue;
-                case 29: continue;
-                case 30: continue;
-                case 31: continue;
+                case 25: IS_DONE(25); sieve[byteIndex] &= MASK0; byteIndex += kValue * 4 + 1; [[fallthrough]];
+                case 26: IS_DONE(26); sieve[byteIndex] &= MASK0; byteIndex += kValue * 2 + 1; [[fallthrough]];
+                case 27: IS_DONE(27); sieve[byteIndex] &= MASK0; byteIndex += kValue * 4 + 2; [[fallthrough]];
+                case 28: IS_DONE(28); sieve[byteIndex] &= MASK0; byteIndex += kValue * 2 + 1; [[fallthrough]];
+                case 29: IS_DONE(29); sieve[byteIndex] &= MASK0; byteIndex += kValue * 4 + 1; [[fallthrough]];
+                case 30: IS_DONE(30); sieve[byteIndex] &= MASK0; byteIndex += kValue * 6 + 3; [[fallthrough]];
+                case 31: IS_DONE(31); sieve[byteIndex] &= MASK0; byteIndex += kValue * 2 + 1; [[fallthrough]];
             }
 
             // handles primes of form 30 * k + 17
             for (;;) {
                 case 32: {
-                            continue;
+                            IS_DONE(32); sieve[byteIndex] &= MASK0; byteIndex += kValue * 6 + 3; [[fallthrough]];
                         }
-                case 33: continue;
-                case 34: continue;
-                case 35: continue;
-                case 36: continue;
-                case 37: continue;
-                case 38: continue;
-                case 39: continue;
+                case 33: IS_DONE(33); sieve[byteIndex] &= MASK0; byteIndex += kValue * 4 + 3; [[fallthrough]];
+                case 34: IS_DONE(34); sieve[byteIndex] &= MASK0; byteIndex += kValue * 2 + 1; [[fallthrough]];
+                case 35: IS_DONE(35); sieve[byteIndex] &= MASK0; byteIndex += kValue * 4 + 2; [[fallthrough]];
+                case 36: IS_DONE(36); sieve[byteIndex] &= MASK0; byteIndex += kValue * 2 + 1; [[fallthrough]];
+                case 37: IS_DONE(37); sieve[byteIndex] &= MASK0; byteIndex += kValue * 4 + 3; [[fallthrough]];
+                case 38: IS_DONE(38); sieve[byteIndex] &= MASK0; byteIndex += kValue * 6 + 3; [[fallthrough]];
+                case 39: IS_DONE(39); sieve[byteIndex] &= MASK0; byteIndex += kValue * 2 + 1; [[fallthrough]];
             }
 
             // handles primes of form 30 * k + 19
             for (;;) {
                 case 40: {
-                            continue;
+                            IS_DONE(40); sieve[byteIndex] &= MASK0; byteIndex += kValue * 6 + 4; [[fallthrough]];
                         }
-                case 41: continue;
-                case 42: continue;
-                case 43: continue;
-                case 44: continue;
-                case 45: continue;
-                case 46: continue;
-                case 47: continue;
+                case 41: IS_DONE(41); sieve[byteIndex] &= MASK0; byteIndex += kValue * 4 + 2; [[fallthrough]];
+                case 42: IS_DONE(42); sieve[byteIndex] &= MASK0; byteIndex += kValue * 2 + 2; [[fallthrough]];
+                case 43: IS_DONE(43); sieve[byteIndex] &= MASK0; byteIndex += kValue * 4 + 2; [[fallthrough]];
+                case 44: IS_DONE(44); sieve[byteIndex] &= MASK0; byteIndex += kValue * 2 + 2; [[fallthrough]];
+                case 45: IS_DONE(45); sieve[byteIndex] &= MASK0; byteIndex += kValue * 4 + 2; [[fallthrough]];
+                case 46: IS_DONE(46); sieve[byteIndex] &= MASK0; byteIndex += kValue * 6 + 4; [[fallthrough]];
+                case 47: IS_DONE(47); sieve[byteIndex] &= MASK0; byteIndex += kValue * 2 + 1; [[fallthrough]];
             }
 
             // handles primes of form 30 * k + 23
             for (;;) {
                 case 48: {
-                            continue;
+                            IS_DONE(48); sieve[byteIndex] &= MASK0; byteIndex += kValue * 6 + 5; [[fallthrough]];
                         }
-                case 49: continue;
-                case 50: continue;
-                case 51: continue;
-                case 52: continue;
-                case 53: continue;
-                case 54: continue;
-                case 55: continue;
+                case 49: IS_DONE(49); sieve[byteIndex] &= MASK0; byteIndex += kValue * 4 + 3; [[fallthrough]];
+                case 50: IS_DONE(50); sieve[byteIndex] &= MASK0; byteIndex += kValue * 2 + 1; [[fallthrough]];
+                case 51: IS_DONE(51); sieve[byteIndex] &= MASK0; byteIndex += kValue * 4 + 4; [[fallthrough]];
+                case 52: IS_DONE(52); sieve[byteIndex] &= MASK0; byteIndex += kValue * 2 + 1; [[fallthrough]];
+                case 53: IS_DONE(53); sieve[byteIndex] &= MASK0; byteIndex += kValue * 4 + 3; [[fallthrough]];
+                case 54: IS_DONE(54); sieve[byteIndex] &= MASK0; byteIndex += kValue * 6 + 5; [[fallthrough]];
+                case 55: IS_DONE(55); sieve[byteIndex] &= MASK0; byteIndex += kValue * 2 + 1; [[fallthrough]];
             }
 
             // handles primes of form 30 * k + 29
             for (;;) {
                 case 56: {
-                            continue;
+                            IS_DONE(56); sieve[byteIndex] &= MASK0; byteIndex += kValue * 6 + 6; [[fallthrough]];
                         }
-                case 57: continue;
-                case 58: continue;
-                case 59: continue;
-                case 60: continue;
-                case 61: continue;
-                case 62: continue;
-                case 63: continue;
+                case 57: IS_DONE(57); sieve[byteIndex] &= MASK0; byteIndex += kValue * 4 + 4; [[fallthrough]];
+                case 58: IS_DONE(58); sieve[byteIndex] &= MASK0; byteIndex += kValue * 2 + 2; [[fallthrough]];
+                case 59: IS_DONE(59); sieve[byteIndex] &= MASK0; byteIndex += kValue * 4 + 4; [[fallthrough]];
+                case 60: IS_DONE(64); sieve[byteIndex] &= MASK0; byteIndex += kValue * 2 + 2; [[fallthrough]];
+                case 61: IS_DONE(64); sieve[byteIndex] &= MASK0; byteIndex += kValue * 4 + 4; [[fallthrough]];
+                case 62: IS_DONE(64); sieve[byteIndex] &= MASK0; byteIndex += kValue * 6 + 6; [[fallthrough]];
+                case 63: IS_DONE(64); sieve[byteIndex] &= MASK0; byteIndex += kValue * 2 + 1; [[fallthrough]];
             }
 
             default: UNREACHABLE;
