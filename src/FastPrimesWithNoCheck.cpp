9d097f1fa98dfe972f682194c883cca5b2543797
projectPythonator <danjkstraProtonAlt1@protonmail.com>
2024-11-16 12:18:19 -0500

    CHANGED added TODOs and the skeleton of switch added in todo markers and switch statement placeholder cases for one modulo class of prime numbers specifically 30 * k + 1 next commit will add rest

--- src/FastPrimesWithNoCheck.cpp
+++ src/FastPrimesWithNoCheck.cpp
@@ -4,10 +4,11 @@
 namespace sieveCompress {
 
 
-// needs class extention
-// typing of sieve needs fixed
+// TODO needs class extention
+// TODO typing of sieve needs fixed
 // in our case I am not sure if sieve size is needed 
 // its here for potential portablility
+// TODO check how expensive cleaning up magic numbers is
 void crossOff(
         uint8_t *sieve, 
         std::size_t sieveSize, 
@@ -18,6 +19,19 @@ void crossOff(
 
         std::size_t maskIndex; // I am split on what type this should have 
         switch (maskIndex) {
+            // current prime is prime % 30 == 1
+            for (;;) {
+                case 0: {
+                            continue;
+                        }
+                case 1: continue;
+                case 2: continue;
+                case 3: continue;
+                case 4: continue;
+                case 5: continue;
+                case 6: continue;
+                case 7: continue;
+            }
 
             default: UNREACHABLE;
         }
