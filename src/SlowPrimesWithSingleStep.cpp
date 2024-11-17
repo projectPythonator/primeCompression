684da781eb4822fcf71d878b81915d6dfdbf9c53
projectPythonator <danjkstraProtonAlt1@protonmail.com>
2024-11-17 16:31:18 -0500

    CHANGED added skeleton code for single step not fully sure if this is what I will go for sure but this is likely it code for both single step medium and big optimization will both use this code with the only difference being which data they end up dealing with the different part might still come from th data structures representing these codes themself not sure yet one might uses ranges and another might use byte lvl lists

--- src/SlowPrimesWithSingleStep.cpp
+++ src/SlowPrimesWithSingleStep.cpp
@@ -4,5 +4,19 @@
 #include "/header/ByteMasks.hpp"
 
 namespace sieveCompress {
+    // only exists here right now as place holder
+    // names are place holder names
+    // if I change currIndex need to track what sieve we are on
+    //      type will change to uint32_t if this happens or uint16_t
+    void updateNumber(uint64_t primeIndex) {
+        uint64_t currIndex = BIG_INDEX[primeIndex]; // might change to local index
+        uint32_t kValue = BIG_K[primeIndex];
+        uint8_t mValue = BIG_MOD[primeIndex];
+        uint8_t mIndex = BIG_MASK_INDEX[primeIndex] & 7;
+        SIEVE[currIndex] &= MASK_LOOKUP[mValue][mIndex];
+        BIG_INDEX[primeIndex] += kValue * SKIP_VALUE[mIndex] + OFFSET_VALUE[mValue][mIndex];
+        BIG_MASK_INDEX[primeIndex]++;
 
-};
+
+    }
+}
