288641641051fdcc70d450835821a327175f897a
projectPythonator <danjkstraProtonAlt1@protonmail.com>
2024-11-19 14:06:16 -0500

    ADDED file for holding info common prime info this info is mainly on primorials, phis, and small primes that we will be using commonly within the project currently am unsure how I want to type these things in this project I will be using unsigned more often then not as I am wanting to utilize mask techniques often that and some of my bounds are based on being about to use the last bit

---
+++ src/header/RBXConstants.hpp
@@ -0,0 +1,14 @@
+#ifndef RBX_CONSTANTS_HPP
+#define RBX_CONSTANTS_HPP
+
+// TODO added the right includes for typing and stuff
+// TODO decide on correct typing of the values
+// TODO use std::array or our::array instead of native
+namespace RBXInfo {
+    constexpr uint8_t rb5_primorial = 30u;  // might need better type
+    constexpr uint8_t rb5_phi = 8u;         // might need better type
+    constexpr uint64_t rbx_primorials[10];  // might need better typing
+    constexpr uint64_t rbx_phis[10];        // might need better typing
+}
+
+#endif
