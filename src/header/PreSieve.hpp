fa1ecee91a86067b87eeb14a5300bb61ec930187
projectPythonator <danjkstraProtonAlt1@protonmail.com>
2024-11-19 13:21:40 -0500

    ADDED file for presieve header added the file it self in added some comments about how I want things to work added in declarations for functions NOTE going to be using c++ core guidelines as the style guide

---
+++ src/header/PreSieve.hpp
@@ -0,0 +1,14 @@
+// place holder
+//
+//
+#include "PreSieveConstArrays.hpp" // pre computed sieve arrays
+
+// TODO move methods to class??
+// TODO measure cost of having applyPreSieve as a helper function vs hardcode
+//  or vs highly optimized span/array decay
+// TODO measure the cost of using 
+namespace SieveCompress {
+
+    void applyPreSieve(std::span<uint8_t>, std::size_t);
+    void applyPreSieves(std::span<uint8_t>);
+}
