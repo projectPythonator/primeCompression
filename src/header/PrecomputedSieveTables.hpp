bba1bf51d52222e2976bd4b9fda3a54c2bd07236
projectPythonator <danjkstraProtonAlt1@protonmail.com>
2024-11-19 13:53:14 -0500

    ADDED file to hold pre computed tables this will just have hard coded arrays coputed for sieving has listed todos in it already

---
+++ src/header/PrecomputedSieveTables.hpp
@@ -0,0 +1,9 @@
+// file mainly going to store precomputed code for lookup tables 
+// will have a python3 script to do this for me
+
+// TODO generate the code via python3
+// TODO add in includes 
+// TODO fix namespace name maybe
+namespace LookupTables {
+
+}
