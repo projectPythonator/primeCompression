ab6f34ebd6d81b63fa8d985e9e91aed486f57450
projectPythonator <danjkstraProtonAlt1@protonmail.com>
2024-11-19 13:19:36 -0500

    CHANGED added TODOs and comments on implementation Added in some new todos for later Added in comments explaining the approach I am going to use Added the definitions of the functions (they might be wrong but fine for now)

--- src/PreSieve.cpp
+++ src/PreSieve.cpp
@@ -2,7 +2,30 @@
 //
 // not sure yet what I will need to includ 
 //
-//
+
+// TODO fix namespace name MAYBE
+// TODO apply these functions to a class based solution
+// TODO move the presieves into this namespace even if they are stored
+//  in anothre file?
 namespace sieveCompress {
+
+    void applyPreSieve(std::span<uint8_t> sieve, std::span<uint8_t> preSieve) {
+        // Step 1: find start point of presieve with math
+        // Step 2: apply the preseive from start
+        // Step 3: loop i = end point of start -> till can't apply full preSieve
+        //      for each iteration apply bitwise ad of presieve to sieve
+        // Step 4: apply the last preseive difference
+        //      can be done in loop at a bit of cost or outside after
+        // Optimization would be doing multiple presieves at once a &= (b & c &d)
+        // or explicitly applying multiple indexes at same time or both
+    }
+
+    void applyPreSieves(std::span<uint8_t> sieve) {
+        // Step 0: fill(sieve, 255U); // make it named literal? optional if not done
+        // Step 1: Loop each preSieve we have
+        //      each iteration call applyPreSieve(sieve, span(next_presieve)) ??
+        // Optimization apply what is said in other function here?
+    }
+
 
 }
