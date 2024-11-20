32dfdcc97ca6f8b69201a8f7263274e575205708
projectPythonator <danjkstraProtonAlt1@protonmail.com>
2024-11-20 13:49:49 -0500

    ADDED source for my file io NOTE: most of the previous globals are being moved to this file in unnamed namespace since they don't belong to be public to the project might need to rethink this part a bit more

---
+++ src/SieveFileIO.cpp
@@ -0,0 +1,18 @@
+
+// TODO add includes
+// TODO add in buffers
+// moved from header to source since I don't think we need to project to see these
+namespace {
+    constexpr std::size_t kibiByte = 1024;
+    constexpr std::size_t page_factor = 32;
+
+    FILE * file_out_stream;
+    FILE * file_in_stream;
+}
+
+
+namespace projectIO {
+
+
+
+}
