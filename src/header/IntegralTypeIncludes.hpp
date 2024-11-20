8a6507cd8c16289055f2e893701e6cad7eceddf6
projectPythonator <danjkstraProtonAlt1@protonmail.com>
2024-11-20 13:40:52 -0500

    ADDED small file to handle fixed and array include

---
+++ src/header/IntegralTypeIncludes.hpp
@@ -0,0 +1,10 @@
+#ifndef INTEGRAL_TYPE_INCLUDES_HPP
+#define INTEGRAL_TYPE_INCLUDES_HPP
+
+#include <cstdint> // for fixed size types
+#include <cstddef> // for array indexing and pointer types
+
+// TODO maybe add namespace idk
+// TODO consider using std::byte over uint8_t
+#endif 
+
