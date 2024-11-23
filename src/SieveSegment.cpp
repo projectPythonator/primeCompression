37ddf99cb86c890c0fbeca5513b3fdea35e4f71d
projectPythonator <danjkstraProtonAlt1@protonmail.com>
2024-11-23 09:17:23 -0500

    ADDED file to tie up all the things added single function NOTE the idea of this function is to tie up all the different segments of sieving into one complete thing NOTE I might do single and multi thread files idk yet

---
+++ src/SieveSegment.cpp
@@ -0,0 +1,16 @@
+#include "header/SieveSegment.hpp"
+
+namespace {
+    // there might need to be a mutex in this
+}
+
+// TODO maybe rename this namespace
+// TODO there is other functions I might be missing but idk
+namespace SieveSegment {
+
+    void sieveSegment(uint64_t bufferBlock) {
+        // idk the specifics of this one yet
+        // needs to load up the tasks here or just before this
+        // 
+    }
+}
