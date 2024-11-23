a330cf280876277316e57f23784e57d5ce5eacd8
projectPythonator <danjkstraProtonAlt1@protonmail.com>
2024-11-23 14:08:02 -0500

    CHANGE added includes and defines TODO need to change defines to delcares added in the includes to the header

--- src/SieveFileIO.cpp
+++ src/SieveFileIO.cpp
@@ -1,6 +1,6 @@
+#include "header/SieveFileIO.hpp"
 
-// TODO add includes
-// TODO add in buffers
+// TODO add in buffers potential
 // moved from header to source since I don't think we need to project to see these
 namespace {
     constexpr std::size_t kibiByte = 1024;
@@ -12,7 +12,20 @@ namespace {
 
 
 namespace projectIO {
+    void setOurBufSize(FILE *);
+    void checkOpenFile(FILE *);
 
+    void writeBlock(std::span<uint8_t>);
+    void readBlock(std::span<uint8_t>);
 
+    void readBlockFromInfoFile(const char *, std::span<uint8_t>);
 
+    void openInStreamInBinaryAndSetBufSize(char *);
+    void openOutStreamInBinaryAndSetBufSize(char *);
+
+    void openInStreamInTextAndSetBufSize(char *);
+    void openOutStreamInTextAndSetBufSize(char *);
+   
+    void readInfoForProgram(const char *, uint64_t &, uint64_t &);
+    void readBufferTillNL(char *, size_t); 
 }
