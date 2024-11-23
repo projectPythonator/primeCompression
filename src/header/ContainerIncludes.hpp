90c9d8c1152b93d6d4c8f547dde320cb923c60f4
projectPythonator <danjkstraProtonAlt1@protonmail.com>
2024-11-23 08:07:37 -0500

    ADDED file to hold includes covers vectors arrays iterators and unordered NOTE once completed we can probably swap them out if needed for performance

---
+++ src/header/ContainerIncludes.hpp
@@ -0,0 +1,9 @@
+#ifndef CONTAINER_INCLUDES_HPP
+#define CONTAINER_INCLUDES_HPP
+
+#include <array>    // might replace this
+#include <iterator> // might not need this
+#include <span>
+#include <vector>   // might replace this
+
+#endif
