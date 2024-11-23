ae26f844af8c1c14167f6bf690aeacfec9f420de
projectPythonator <danjkstraProtonAlt1@protonmail.com>
2024-11-23 12:51:59 -0500

    CHANGED fixed todos and includes added includes just in case removed comments fixed namespace name

--- src/header/PreSieve.hpp
+++ src/header/PreSieve.hpp
@@ -1,16 +1,16 @@
-// place holder
-//
-//
 #ifndef PRE_SIEVE_HPP
 #define PRE_SIEVE_HPP
 
-#include "PreSieveConstArrays.hpp" // pre computed sieve arrays
+// need to measure how expensive it is 
+#include "ContainerIncludes.hpp"
+#include "IntegralTypeIncludes.hpp"
+#include "PresieveComputedLookupTables.hpp" // pre computed sieve arrays
 
-// TODO move methods to class??
+// TODO measure the cost of using 
+// TODO move methods to class?
 // TODO measure cost of having applyPreSieve as a helper function vs hardcode
 //  or vs highly optimized span/array decay
-// TODO measure the cost of using 
-namespace SieveCompress {
+namespace PreSieve {
     void applyPreSieve(std::span<uint8_t>, std::size_t);
     void applyPreSieves(std::span<uint8_t>);
 }
