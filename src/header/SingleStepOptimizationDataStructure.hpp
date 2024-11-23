2d2e6394726b6dc023968275bd0d71b93ebe93b1
projectPythonator <danjkstraProtonAlt1@protonmail.com>
2024-11-23 07:47:15 -0500

    CHANGED I forgot lists can be represented as array removed all functions planned added locked and unlocked setting list head added locked and unlocked getting list head NOTE: so we are going to go with lists being arrays point to the next note with an index and as such we can just use a list of indexes that represents heads in this structure REASON 1: our previous node idea was have nodes just have indexs to the positions, so we safe space on needing pointers REASON 2: our total needed memory needed doesn't change. REASON 3: it should be faster to update a integral at an array index vs updating a node next, it is also more simple. REASON 4: same with 3 above updating and clearing an integer in the single step data structure should be more simple and potentially faster REASON 5: so basically should be faster, it is more simple, and takes less memory

--- src/header/SingleStepOptimizationDataStructure.hpp
+++ src/header/SingleStepOptimizationDataStructure.hpp
@@ -1,21 +1,26 @@
 // adding this file in for the ds that manages multiple linked lists
 
+#ifndef SINGLE_STEP_OPTIMIZATION_HPP
+#define SINGLE_STEP_OPTIMIZATION_HPP
+// #include "./header/IntegralIncludes.hpp"
+// #include "./header/ContainerIncludes.hpp" // stays in the header?
+
+// TODO add includes
 // TODO fix the naming of the namespace
 // TODO make the functoins into class methods maybe
+// NOTE 1: locked is for when we have each thread update its own nodes as part 
+//  of the loop process.
+// NOTE 2: unlocked is for single thread or when we do batch updates since 
+//  batches single threaded, we might need a mutex but will consider that when
+//  closer to done and when I got to compare as its external.
 namespace PlaceHolder {
+    uint32_t getHeadValueLocked(uint32_t);
+    uint32_t getHeadValueUnlocked(uint32_t);
 
-    void updatePrimeRecordInfo(std::size_t);
-    // this function might need a span/array if I make it generalized
-    void updatePrimeRecordPlacement(std::size_t); 
-
-    //might be missing one or two parameters
-    void sievePrimesInRange(std::span<std::size_t>);
-
-    // class vars
-    // mutex maybe
-    // likely want to hold the bigger sieves in the class
-    // maybe have what range each element of each block of sieves is represented as lists
-    //  - for the smaller sized ones I was planning on maybe doing it per byte
-    //  - for the bigger ones I was planning on doing each big sieve as a section of its own smaller ones
-    // might be forgeting some
+    // need to profile this to see how expensive it is
+    // inline or hardcode if too expensive
+    uint32_t setHeadValueLocked(uint32_t, uint32_t);
+    uint32_t setHeadValueUnlocked(uint32_t, uint32_t);
 }
+
+#endif
