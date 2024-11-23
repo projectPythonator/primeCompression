bade2dfe2d59bdcc3770908a25f8fa219b7f1f7d
projectPythonator <danjkstraProtonAlt1@protonmail.com>
2024-11-23 18:21:37 -0500

    CHANGED added the slow prime vectors this time

--- src/header/PrimeInfoRecords.hpp
+++ src/header/PrimeInfoRecords.hpp
@@ -9,7 +9,12 @@ namespace PrimeInfo {
     std::array<uint8_t, BoundConstants::fast_small_prime_amt> fast_primes_mask;
     std::array<uint8_t, BoundConstants::fast_small_prime_amt> fast_primes_mask_pos;
     std::array<uint16_t, BoundConstants::fast_small_prime_amt> fast_primes_kvalue;
-    std::array<uint64_t, BoundConstants::fast_small_prime_amt> fast_primes_position;
+    std::array<uint64_t, BoundConstants::fast_small_prime_amt> fast_primes_pos;
+
+    std::vector<uint8_t> slow_primes_mask;
+    std::vector<uint8_t> slow_primes_mask_pos;
+    std::vector<uint32_t> slow_primes_kvalue;
+    std::vector<uint32_t> slow_primes_prime_pos;
 }
 
 
