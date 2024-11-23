254c66ca8a59ba8b5af66d150e37b0c1806e2140
projectPythonator <danjkstraProtonAlt1@protonmail.com>
2024-11-23 14:00:52 -0500

    ADDED header file added namespace added defines added TODOs might move it to the namespace added includes

---
+++ src/header/FastPrimesWithNoCheck.hpp
@@ -0,0 +1,16 @@
+// not sure what includes I will need for this file
+#include "ByteMasks.hpp"
+#include "ContainerIncludes.hpp"
+#include "IntegralTypeIncludes.hpp"
+
+// TODO needs class extention
+// TODO change sieve to span if possible removes size param
+// TODO check how expensive cleaning up magic numbers is
+// TODO might need to switch this function up if too slow
+// TODO change maskIndex typing maybe
+// TODO apply correct masks
+// TODO missing functions 
+namespace SieveCompress {
+    void crossOff(uint8_t *, std::size_t, std::size_t, std::size_t);
+}
+
