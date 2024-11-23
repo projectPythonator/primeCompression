44197c8d41bf99631a860a2d8e1e8098717f44fb
projectPythonator <danjkstraProtonAlt1@protonmail.com>
2024-11-23 15:10:19 -0500

    CHANGED to not hold comments

--- src/header/PrecomputedSieveTables.hpp
+++ src/header/PrecomputedSieveTables.hpp
@@ -1,13 +1,9 @@
-// file mainly going to store precomputed code for lookup tables 
-// will have a python3 script to do this for me
-
 #ifndef PRECOMPUTED_LOOKUP_TABLES_HPP
 #define PRECOMPUTED_LOOKUP_TABLES_HPP
 
 #include "ContainerIncludes.hpp"
 #include "IntegralTypeIncludes.hpp"
 
-// TODO generate the code via python3
 namespace PresieveLookup {
 
 }
