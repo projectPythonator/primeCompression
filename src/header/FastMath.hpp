0e3d3c7ef2c088120a509499d8b20f7f5fc00bf8
projectPythonator <danjkstraProtonAlt1@protonmail.com>
2024-11-23 08:02:20 -0500

    CHANGE style guide fixing changed typecase in div functions changed constexpr from uint64_t to __uint128_t to accomadate the removal of the typecast in functions REASON: apparently this is the proper way when you have the change to make the literal casted

--- src/header/FastMath.hpp
+++ src/header/FastMath.hpp
@@ -5,23 +5,26 @@
 #ifndef FAST_MATH_HPP
 #define FAST_MATH_HPP
 
+// TODO does it make more sense
+// TODO check if it makes more sense do the inv consts as __uint128s
 namespace {
     // used for normalizing the inverse function
     constexpr uint64_t NORMALIZE_SHIFT = 68ULL;  
     constexpr uint64_t NORMALIZE_MUL = 30ULL;
 
     // used for fast div and mod by 30 difference of 1 to accound for rounding
-    constexpr uint64_t INVERSE_DIV_30_LITERAL = 9838263505978427528ULL; 
-    constexpr uint64_t INVERSE_MOD_30_LITERAL = 9838263505978427529ULL; 
+    constexpr __uint128_t INVERSE_DIV_30_LITERAL = 9838263505978427528ULL; 
+    constexpr __uint128_t INVERSE_MOD_30_LITERAL = 9838263505978427529ULL; 
 }
 
 uint64_t fastDivideBy30(uint64_t n) {
-    return (__uint128_t (n) * INVERSE_DIV_30_LITERAL) >> NORMALIZE_SHIFT;
+    return (n * INVERSE_DIV_30_LITERAL) >> NORMALIZE_SHIFT;
 }
 
 uint64_t fastModBy30(uint64_t n) {
-    return n - ((__uint128_t (n) * INVERSE_MOD_30_LITERAL) >> NORMALIZE_SHIFT) * NORMALIZE_MUL;
+    return n - ((n * INVERSE_MOD_30_LITERAL) >> NORMALIZE_SHIFT) * NORMALIZE_MUL;
 }
+
 
 bool cmpSquares(uint64_t m, uint16_t n) {
     return m <= n * n;
