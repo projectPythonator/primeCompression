6ddc9d409e0fdf0b4524b871d8e86753e058d898
projectPythonator <danjkstraProtonAlt1@protonmail.com>
2024-11-23 09:21:34 -0500

    ADDED added the source file for header

---
+++ src/header/SieveSegment.hpp
@@ -0,0 +1,11 @@
+#ifndef SIEVE_SEGMENT_HPP
+#define SIEVE_SEGMENT_HPP
+
+// TODO fix the namespace name
+// TODO i know I am missing a few functions because I don't have the specifics down
+// TODO fix includes
+namespace SieveSegment {
+    void sieveSegment(uint64_t);
+}
+
+#endif
