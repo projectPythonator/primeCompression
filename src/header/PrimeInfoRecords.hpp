6bf513fe425111e57d044830bb1686589a9f1ebb
projectPythonator <danjkstraProtonAlt1@protonmail.com>
2024-11-23 14:43:47 -0500

    ADDED file to hold prime record info this is the file we will be dumping follwing into prime mod current mod index prime kvalue curent prime block (might be a start point or a absolute value)

---
+++ src/header/PrimeInfoRecords.hpp
@@ -0,0 +1,14 @@
+#ifndef PRIME_INFO_RECORDS_HPP
+#define PRIME_INFO_RECORDS_HPP
+
+#include "ContainerIncludes.hpp"
+#include "IntegralTypeIncludes.hpp"
+#include "SieveConstants.hpp"
+
+namespace PrimeInfo {
+
+
+}
+
+
+#endif
