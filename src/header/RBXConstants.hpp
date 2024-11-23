9636aa3edf09da4ea5ca3939cd0c5b7334e83c89
projectPythonator <danjkstraProtonAlt1@protonmail.com>
2024-11-23 12:55:44 -0500

    CHANGE fixed includes and todos
    removed TODOs that got fixed
    added includes
    reordered the namespace though idk if it matters

--- src/header/RBXConstants.hpp
+++ src/header/RBXConstants.hpp
@@ -1,14 +1,16 @@
 #ifndef RBX_CONSTANTS_HPP
 #define RBX_CONSTANTS_HPP
 
-// TODO added the right includes for typing and stuff
+#include "ContainerIncludes.hpp" // for array
+#include "IntegralTypeIncludes.hpp"
+
 // TODO decide on correct typing of the values
 // TODO use std::array or our::array instead of native
 namespace RBXInfo {
-    constexpr uint8_t rb5_primorial = 30u;  // might need better type
-    constexpr uint8_t rb5_phi = 8u;         // might need better type
     constexpr uint64_t rbx_primorials[10];  // might need better typing
     constexpr uint64_t rbx_phis[10];        // might need better typing
+    constexpr uint8_t rb5_primorial = 30u;  // might need better type
+    constexpr uint8_t rb5_phi = 8u;         // might need better type
 }
 
 #endif
