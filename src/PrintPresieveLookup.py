e7ed5b35ec0846cef27c5a85d29b5f399caca06e
projectPythonator <danjkstraProtonAlt1@protonmail.com>
2024-11-24 13:51:17 -0500

    CHANGED updated file to print the lookup code need to test the code since can't remember if I generated the position correctly

--- src/PrintPresieveLookup.py
+++ src/PrintPresieveLookup.py
@@ -7,6 +7,8 @@ def startSide():
     print('#include "ContainerIncludes.hpp"')
     print('#include "IntegralTypeIncludes.hpp"')
     print()
+    print('// generated with PrintPresieveLookup.py in src')
+    print()
     print('namespace PresieveLookup {')
     print()
 
@@ -17,20 +19,70 @@ def endSide():
     print('#endif')
     print()
 
-def printSievePart(sieves):
-    # needs to print some kinda formatting
-    pass
+def sievePrime(prime, marks, primorial):
+    jumps = [1, 7, 11, 13, 17, 19, 23, 29]
+    for k in range(100000):
+        for jump in jumps:
+            abs_pos = prime * k * 30 + jump * prime
+            if abs_pos >= primorial:
+                return
+            marks.add(abs_pos)
 
-def midSide(sieves):
+def printSievePart(primes):
+    mods = {1:1, 7:2, 11:4, 13:8, 17:16, 19:32, 23:64, 29:128}
+    primorial = 1
+    for p in primes:
+        primorial *= p
+    marks = set()
+    for p in primes:
+        sievePrime(p, marks, primorial)
+    sieve = [255] * primorial
+    for mark in marks:
+        block = mark // 30
+        mask = ~mods[mark % 30]
+        sieve[block] &= mask
+    strOut = '  { '
+    for ind, b in enumerate(sieve):
+        if ind > 0 and ind % 20 == 0:
+            print(strOut)
+            strOut = '    '
+        strOut += "0x{:02x}".format(b)
+        if ind != len(sieve)-1:
+            strOut += ', '
+        else:
+            strOut += '}'
+    return strOut
+
+def midSide(primess):
     print('constexpr std::array<std::array<uint8_t>, 16u> presieve_lookup = {\n')
-
-    print('}\n')
-
-
+    for ind, primes in enumerate(primess):
+        strOut = printSievePart(primes)
+        if ind != len(primess) - 1:
+            strOut += ','
+        print(strOut)
+    print('}};')
 
 def main():
+    primess = [
+            [7,  23, 37], 
+            [11, 19, 31], 
+            [13, 17, 29], 
+            [41, 163], 
+            [43, 157], 
+            [47, 151], 
+            [53, 149], 
+            [59, 139], 
+            [61, 137], 
+            [67, 131], 
+            [71, 127], 
+            [73, 113], 
+            [79, 109], 
+            [83, 107], 
+            [89, 103], 
+            [97, 101]] 
     startSide()
-
+    midSide(primess)
     endSide()
 
+main()
 
