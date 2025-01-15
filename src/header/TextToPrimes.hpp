183a58fcb1ebc22a31afa6efc319cc647fe36cd6
projectPythonator <danjkstraProtonAlt1@protonmail.com>
2025-01-15 07:26:00 -0500

    Added in related header file that was missing

---
+++ src/header/TextToPrimes.hpp
@@ -0,0 +1,14 @@
+#ifndef TEXT_TO_PRIMES_HPP
+#define TEXT_TO_PRIMES_HPP
+
+#include "ContainerIncludes.hpp"
+#include "IntegralTypeIncludes.hpp"
+
+
+namespace EndPointConversions {
+    // change this to 3 functions with asserts, 9, 16, 20
+    std::uint64_t getNextNumberBase10(std::span<const std::uint8_t>);
+
+    void convertTextBlock(std::span<const std::uint8_t>, std::span<const std::size_t>, std::span<std::uint64_t>);
+}
+#endif
