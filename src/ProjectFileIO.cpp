24fa5299885fb9dd26e781184537b9eae5ee09d4
projectPythonator <danjkstraProtonAlt1@protonmail.com>
2025-01-16 07:32:33 -0500

    changed up my io file CHANGED a lot of the naming SPANS now using spans for lots of things ERRORS some of the naming got messed up so need to fix it still PROFILING need to profile the functions to see if its kinda slow NOTE the changes should allow for easier handling with multiple threads NOTE am choosing to adjust all ascii text by zero character this does have a cost but shouldn't be too bad, it allows the code in the translating text to numbers to be more simple

--- src/ProjectFileIO.cpp
+++ src/ProjectFileIO.cpp
@@ -1,19 +1,32 @@
-#include "header/SieveFileIO.hpp"
+#include "header/ProjectFileIO.hpp"
+#include <cassert>
 
 // TODO add in buffers potential
 // moved from header to source since I don't think we need to project to see these
 namespace {
-    constexpr std::size_t kibiByte = 1024;
+    constexpr std::uint8_t unsigned_zero = '0';
+    constexpr std::uint8_t unsigned_new_line = '\n';
+
+    constexpr std::size_t kibi_byte = 1024;
     constexpr std::size_t page_factor = 32;
+    constexpr std::size_t max_20_digit = 21;
 
     FILE * file_out_stream;
     FILE * file_in_stream;
+
+    void remove_zero_factor(std::span<std::uint8_t> buf) {
+        std::for_each(buf.begin(), buf.end(), [](std::uint8_t &el) { el -= unsigned_zero; });
+    }
+
+    void add_zero_factor(std::span<std::uint8_t> buf) {
+        std::for_each(buf.begin(), buf.end(), [](std::uint8_t &el) { el += unsigned_zero; });
+    }
 }
 
 
 namespace projectIO {
     void setOurBufSize(FILE *fileStream) {
-        if (std::setvbuf(fileStream, nullptr, _IOFBF, KIBIBYTE * PAGE_FACTOR))
+        if (std::setvbuf(fileStream, nullptr, _IOFBF, kibi_byte * page_factor))
             std::perror("failed to resize OUT stream size\n");
     }
 
@@ -22,120 +35,114 @@ namespace projectIO {
             std::perror("failed to set associated file stream to value\n");
     }
 
-    void writeBuffer(uint8_t *buf, size_t bufSize) {
-        //std::fwrite(buf, sizeof(uint8_t), bufSize, FILE_OUT_STREAM);
-        fwrite_unlocked(buf, sizeof(uint8_t), bufSize, FILE_OUT_STREAM);
+    void writeBlock_1Byte(std::span<std::uint8_t> buf) {
+        //fwrite_unlocked(buf.data(), 1u, buf.size(), file_out_system);
+        add_zero_factor(buf);
+        std::fwrite(buf.data(), 1u, buf.size(), file_out_system);
     }
 
-    void writeBuffer_uint64_t(uint64_t *buf, size_t bufSize) {
-        //std::fwrite(buf, sizeof(uint64_t), bufSize, FILE_OUT_STREAM);
-        fwrite_unlocked(buf, sizeof(uint64_t), bufSize, FILE_OUT_STREAM);
+    void writeBlock_8Byte(std::span<std::uint64_t> buf) {
+        //fwrite_unlocked(buf.data(), 8u, buf.size(), file_out_system);
+        std::fwrite(buf.data(), 8u, buf.size(), file_out_system);
     }
 
-
-    void readBuffer(uint8_t *buf, size_t bufSize) {
-        //std::fread(buf, sizeof(uint8_t), bufSize, FILE_IN_STREAM);
-        fread_unlocked(buf, sizeof(uint8_t), bufSize, FILE_IN_STREAM);
-        EOF_NOT_READ = (0 == std::feof(FILE_IN_STREAM));
+    void flushBuffer(std::span<std::uint8_t> buf) {
+        assert(buf.size() > 0);
+        std::size_t lastIndex = buf.size() - 1;
+        for (; lastIndex > 0; p--)
+            if (buf[lastIndex])
+                break;
+        writeBlock_1Byte(buf.subspan(0, lastIndex));
     }
 
-    void readBuffer_uint64_t(uint64_t *buf, size_t bufSize) {
-        //std::fread(buf, sizeof(uint64_t), bufSize, FILE_IN_STREAM);
-        fread_unlocked(buf, sizeof(uint64_t), bufSize, FILE_IN_STREAM);
-        EOF_NOT_READ = (0 == std::feof(FILE_IN_STREAM));
+    std::size_t readTillNL(std::span<std::uint8_t> buf) {
+        //std::size_t bytesRead = fread_unlocked(buf.data(), 1u, buf.size(), FILE_IN_STREAM);
+        assert(buf.size() < max_20_digit);
+        std::size_t bytesRead = 0u;
+        int chr = fgetc_unlocked(file_in_stream);
+        for(; (bytesRead < buf.size()) && (chr != '\n' && chr != EOF); bytesRead++) {
+            buf[bytesRead] = chr;
+            chr = fgetc_unlocked(file_in_stream);
+        }
+        buf[bytesRead] = unsigned_new_line;
+        eof_not_read = (chr != EOF);
+        return bytesRead;
     }
 
-    void readBlockFromInfoFile(const char *fileName, uint8_t *block, size_t dataAmt) {
-        char dataFileName[256] = "./data/";
-        std::strcat(dataFileName, fileName);
-        auto f = fopen(dataFileName, "rb");
-        std::fread(block, 1, dataAmt, f);
-        fclose(f);
+    std::size_t readBlock_1Byte(std::span<std::uint8_t> buf) {
+        //std::size_t bytesRead = fread_unlocked(buf.data(), 1u, buf.size(), FILE_IN_STREAM);
+        std::size_t bytesRead = std::fread(buf.data(), 1u, buf.size(), FILE_IN_STREAM);
+        eof_not_read = (0 == std::feof(file_in_stream));
+        return bytesRead;
     }
 
-    void openInStreamInBinaryAndSetBufSize(char *fileName) {
-        if (0 == std::strncmp(fileName, "STDIN\0", 5)) {
-            FILE_IN_STREAM = std::freopen(nullptr, "rb", stdin);
+    std::size_t readblock_8byte(std::span<std::uint64_t> buf) {
+        //std::size_t bytesread = fread_unlocked(buf.data(), 8u, buf.size(), file_in_stream);
+        std::size_t bytesread = std::fread(buf.data(), 8u, buf.size(), file_in_stream);
+        eof_not_read = (0 == std::feof(file_in_stream));
+        return bytesread;
+    }
+
+    std::size_t readBlockTillNL(std::span<std::uint8_t> buf) {
+        std::size_t bytesread = readblock_1byte(buf.supspan(0, buf.size() - max_20_digit));
+        if (eof_not_read && buf[readbytes-1] != '\n') 
+            std::size_t bytesread += readtillnl(buf.supspan(buf.size() - max_20_digit, buf.size()));
+        remove_zero_factor(buf);
+        return bytesread;
+    }
+
+    std::size_t readBlocknBytes(std::span<std::uint8_t> buf) {
+        std::size_t bytesRead = readBlock_1Byte(buf);
+        remove_zero_factor(buf);
+        return bytesRead;
+    }
+
+    void openinstreaminbinaryandsetbufsize(char *filename) {
+        if (0 == std::strncmp(filename, "stdin\0", 5)) {
+            file_in_stream = std::freopen(nullptr, "rb", stdin);
         } else {
-            FILE_IN_STREAM = std::fopen(fileName, "rb");
+            file_in_stream = std::fopen(filename, "rb");
         }
     }
 
-    void openOutStreamInBinaryAndSetBufSize(char *fileName) {
-        if (0 == std::strncmp(fileName, "STDOUT\0", 6)) {
-            FILE_OUT_STREAM = std::freopen(nullptr, "wb", stdout);
+    void openoutstreaminbinaryandsetbufsize(char *filename) {
+        if (0 == std::strncmp(filename, "stdout\0", 6)) {
+            file_out_stream = std::freopen(nullptr, "wb", stdout);
         } else {
-            FILE_OUT_STREAM = std::fopen(fileName, "wb+");
+            file_out_stream = std::fopen(filename, "wb+");
         }
     }
 
-    void openInStreamInTextAndSetBufSize(char *fileName) {
-        if (0 == std::strncmp(fileName, "STDIN\0", 5)) {
-            FILE_IN_STREAM = std::freopen(nullptr, "r", stdin);
+    void openinstreamintextandsetbufsize(char *filename) {
+        if (0 == std::strncmp(filename, "stdin\0", 5)) {
+            file_in_stream = std::freopen(nullptr, "r", stdin);
         } else {
-            FILE_IN_STREAM = std::fopen(fileName, "r");
+            file_in_stream = std::fopen(filename, "r");
         }
     }
 
-    void openOutStreamInTextAndSetBufSize(char *fileName) {
-        if (0 == std::strncmp(fileName, "STDOUT\0", 6)) {
-            FILE_OUT_STREAM = std::freopen(nullptr, "w", stdout);
+    void openoutstreamintextandsetbufsize(char *filename) {
+        if (0 == std::strncmp(filename, "stdout\0", 6)) {
+            file_out_stream = std::freopen(nullptr, "w", stdout);
         } else {
-            FILE_OUT_STREAM = std::fopen(fileName, "w+");
+            file_out_stream = std::fopen(filename, "w+");
         }
     }
 
-    void readInfoForProgram(const char *fileName, uint64_t &data1, uint64_t &data2) {
-        char dataFileName[256] = "./data/";
-        char outFileName[100];
-        char inFileName[100];
-        std::strcat(dataFileName, fileName);
-        auto f = std::fopen(dataFileName, "r");
-        if (nullptr == f)
-            std::perror("failed to open arguements file for program\n");
-        std::fscanf(f, "%" SCNu64 "\n", &data1);
-        std::fscanf(f, "%" SCNu64 "\n", &data2);
-        if (nullptr == std::fgets(outFileName, sizeof(outFileName), f)) {
-            std::perror("failed to read the out stream file from args file\n");
-            return;
-        }
-        if (nullptr == std::fgets(inFileName, sizeof(inFileName), f)) {
-            std::perror("failed to read the in stream file from args file\n");
-            return;
-        }
-        std::fclose(f);
-        outFileName[std::strcspn(outFileName, "\n")] = '\0';
-        inFileName[std::strcspn(inFileName, "\n")] = '\0';
-        openOutStreamInBinaryAndSetBufSize(outFileName);
-        checkOpenFile(FILE_OUT_STREAM);
-        openInStreamInBinaryAndSetBufSize(inFileName);
-        checkOpenFile(FILE_IN_STREAM);
-        setOurBufSize(FILE_IN_STREAM);
-        setOurBufSize(FILE_OUT_STREAM);
-    }
+    void readinfoforprogram(std::span<char> infilename, std::span<char> outfilename) {
+        assert(infilename.size()  < filename_max);
+        assert(outfilename.size() < filename_max);
 
-    void flushBuffer(uint8_t *buf, size_t bufSize) {
-        size_t lastIndex = 0;
-        for (size_t p = 0; p < bufSize; p++)
-            if (buf[p])
-                lastIndex = p;
-        writeBuffer(buf, lastIndex);
-    }
+        outfilename[std::strcspn(outfilename, "\n")] = '\0';
+        infilename[std::strcspn(infilename, "\n")] = '\0';
 
-    void readBufferTillNL(char *buf, size_t bufSize) {
-        size_t readBytes = fread_unlocked(buf, 1, bufSize, FILE_IN_STREAM);
-        EOF_NOT_READ = (0 == std::feof(FILE_IN_STREAM));
-        if (EOF_NOT_READ && buf[readBytes-1] != '\n') {
-            int chr = fgetc_unlocked(FILE_IN_STREAM);
-            for (; chr != '\n' && chr != EOF; chr = fgetc_unlocked(FILE_IN_STREAM))
-                buf[readBytes++] = chr;
-            buf[readBytes] = '\n';
-            EOF_NOT_READ = (chr != EOF);
-        }
-        //return readBytes;
+        openoutstreaminbinaryandsetbufsize(outfilename.data());
+        checkopenfile(file_out_stream);
+        openinstreaminbinaryandsetbufsize(infilename.data());
+        checkopenfile(file_in_stream);
+
+        setourbufsize(file_in_stream);
+        setourbufsize(file_out_stream);
     }
 }
-
-#include "rb5FileIOHeader.h"
-
 
--- src/header/ProjectFileIO.hpp
+++ src/header/ProjectFileIO.hpp
@@ -8,30 +8,30 @@
 // TODO better namespace name
 // TODO fix typing am going to migrate to using fstream idk how good or bad it will be
 namespace projectIO {
-    // does this instead need to be static??
-    inline bool EOF_NOT_READ = true;
-
-    // fix the typing on this
+    inline bool eof_not_read = true;
+    
     void setOurBufSize(FILE *);
     void checkOpenFile(FILE *);
 
-    void writeBlock(std::span<uint8_t>);
-    void readBlock(std::span<uint8_t>);
+    void writeBlock_1Byte(std::span<std::uint8_t>);
+    void writeBlock_8Byte(std::span<std::uint64_t>);
+    void flushBuffer(std::span<std::uint8_t>);
 
-    void readBlockFromInfoFile(const char *, std::span<uint8_t>);
+    std::size_t readTillNL(std::span<std::uint8_t>);
+    std::size_t readBlock_1Byte(std::span<std::uint8_t>);
 
-    void openInStreamInBinaryAndSetBufSize(char *);
-    void openOutStreamInBinaryAndSetBufSize(char *);
+    std::size_t readblocktillnl(std::span<std::uint8_t>);
+    std::size_t readblocknbytes(std::span<std::uint8_t>);
 
-    void openInStreamInTextAndSetBufSize(char *);
-    void openOutStreamInTextAndSetBufSize(char *);
+    std::size_t readblock_8byte(std::span<std::uint64_t>); 
 
-    void openFiles(const char *, const char *);
-   
-    void readInfoForProgram(const char *, uint64_t &, uint64_t &);
-    void readBufferTillNL(char *, size_t); 
+    void openinstreaminbinaryandsetbufsize(char *); 
+    void openoutstreaminbinaryandsetbufsize(char *); 
+    void openinstreamintextandsetbufsize(char *);
+    void openoutstreamintextandsetbufsize(char *);
+
+    void readinfoforprogram(std::span<char>, std::span<char>);
 }
-
 // keeping this in case it happens to be faster
 /*
 void setOurBufSize(FILE *);
