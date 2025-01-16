4690a68f94bf574894852e221f61c77594cf1e29
projectPythonator <danjkstraProtonAlt1@protonmail.com>
2025-01-16 18:13:34 -0500

    changed up the way I open files and added func new func is for reading a block of text I shall read this way

--- src/ProjectFileIO.cpp
+++ src/ProjectFileIO.cpp
@@ -5,19 +5,30 @@
 namespace {
     constexpr std::uint8_t unsigned_zero = '0';
     constexpr std::uint8_t unsigned_new_line = '\n';
+    constexpr std::uint8_t unsigned_line_break = unsigned_new_line - unsigned_zero;
+
 
     constexpr std::size_t kibi_byte = 1024;
     constexpr std::size_t page_factor = 32;
     constexpr std::size_t max_20_digit = 21;
 
-    FILE * file_out_stream;
-    FILE * file_in_stream;
+    constexpr std::size_t text_block_max_numbers = kibi_byte * 4u;
+    constexpr std::size_t text_block_size = kibi_byte * page_factor;
 
-    void remove_zero_factor(std::span<std::uint8_t> buf) {
+    constexpr char wb_mode[3] = "wb";
+    constexpr char rb_mode[3] = "rb";
+    constexpr char wt_mode[3] = "w+";
+    constexpr char rt_mode[2] = "r";
+
+    FILE * file_out_stream = stdout;
+    FILE * file_in_stream = stdin;
+
+    void remove_zero_factor(const std::span<std::uint8_t> buf) {
+        assert(buf.size() % 16 == 0);
         std::for_each(buf.begin(), buf.end(), [](std::uint8_t &el) { el -= unsigned_zero; });
     }
 
-    void add_zero_factor(std::span<std::uint8_t> buf) {
+    void add_zero_factor(const std::span<std::uint8_t> buf) {
         std::for_each(buf.begin(), buf.end(), [](std::uint8_t &el) { el += unsigned_zero; });
     }
 }
@@ -34,31 +45,29 @@ namespace projectIO {
             std::perror("failed to set associated file stream to value\n");
     }
 
-    void writeBlock_1Byte(std::span<std::uint8_t> buf) {
-        //fwrite_unlocked(buf.data(), 1u, buf.size(), file_out_system);
-        std::fwrite(buf.data(), 1u, buf.size(), file_out_system);
+    void writeBlock_1Byte(const std::span<const std::uint8_t> buf) {
+        std::fwrite(buf.data(), 1u, buf.size(), file_out_stream);
     }
 
-    void writeBlock_8Byte(std::span<std::uint64_t> buf) {
-        //fwrite_unlocked(buf.data(), 8u, buf.size(), file_out_system);
-        std::fwrite(buf.data(), 8u, buf.size(), file_out_system);
+    void writeBlock_8Byte(const std::span<const std::uint64_t> buf) {
+        std::fwrite(buf.data(), 8u, buf.size(), file_out_stream);
     }
 
-    void writeBlockOfText(std::span<std::uint8_t> buf) {
+    void writeBlockOfText(const std::span<std::uint8_t> buf) {
         add_zero_factor(buf);
         writeBlock_1Byte(buf);
     }
 
-    void flushBuffer(std::span<std::uint8_t> buf) {
+    void flushBuffer(const std::span<const std::uint8_t> buf) {
         assert(buf.size() > 0);
         std::size_t lastIndex = buf.size() - 1;
-        for (; lastIndex > 0; p--)
+        for (; lastIndex > 0; lastIndex--)
             if (buf[lastIndex])
                 break;
         writeBlock_1Byte(buf.subspan(0, lastIndex));
     }
 
-    std::size_t readTillNL(std::span<std::uint8_t> buf) {
+    std::size_t readTillNL(const std::span<std::uint8_t> buf) {
         //std::size_t bytesRead = fread_unlocked(buf.data(), 1u, buf.size(), FILE_IN_STREAM);
         assert(buf.size() < max_20_digit);
         std::size_t bytesRead = 0u;
@@ -72,92 +81,83 @@ namespace projectIO {
         return bytesRead;
     }
 
-    std::size_t readBlock_1Byte(std::span<std::uint8_t> buf) {
-        //std::size_t bytesRead = fread_unlocked(buf.data(), 1u, buf.size(), FILE_IN_STREAM);
-        std::size_t bytesRead = std::fread(buf.data(), 1u, buf.size(), FILE_IN_STREAM);
+    std::size_t readBlock_1Byte(const std::span<std::uint8_t> buf) {
+        //std::size_t bytesRead = fread_unlocked(buf.data(), 1u, buf.size(), file_in_stream);
+        std::size_t bytesRead = std::fread(buf.data(), 1u, buf.size(), file_in_stream);
         eof_not_read = (0 == std::feof(file_in_stream));
         return bytesRead;
     }
 
-    std::size_t readBlock_8Byte(std::span<std::uint64_t> buf) {
+    std::size_t readBlock_8Byte(const std::span<std::uint64_t> buf) {
         //std::size_t bytesread = fread_unlocked(buf.data(), 8u, buf.size(), file_in_stream);
         std::size_t bytesread = std::fread(buf.data(), 8u, buf.size(), file_in_stream);
         eof_not_read = (0 == std::feof(file_in_stream));
         return bytesread;
     }
 
-    std::size_t readBlockTillNL(std::span<std::uint8_t> buf) {
-        std::size_t bytesread = readblock_1byte(buf.supspan(0, buf.size() - max_20_digit));
-        if (eof_not_read && buf[readbytes-1] != '\n') 
-            std::size_t bytesread += readtillnl(buf.supspan(buf.size() - max_20_digit, buf.size()));
-        remove_zero_factor(buf);
-        return bytesread;
-    }
-
-    std::size_t readBlockNBytes(std::span<std::uint8_t> buf) {
+    std::size_t readBlockNBytes(const std::span<std::uint8_t> buf) {
         std::size_t bytesRead = readBlock_1Byte(buf);
         remove_zero_factor(buf);
         return bytesRead;
     }
 
-    void openInStreamInBinaryAndSetBufSize(char *filename) {
-        if (0 == std::strncmp(filename, "stdin\0", 5)) {
-            file_in_stream = std::freopen(nullptr, "rb", stdin);
+    FILE * openStreamInGivenMode(const std::span<const char> fileName, const std::span<const char> fileMode, FILE *stream) {
+        if (0 == std::strncmp(fileName.data(), "stdin\0", 5)) {
+            stream = std::freopen(nullptr, fileMode.data(), stdin);
+        } else if (0 == std::strncmp(fileName.data(), "stdout\0", 6)) {
+            stream = std::freopen(nullptr, fileMode.data(), stdout);
         } else {
-            file_in_stream = std::fopen(filename, "rb");
+            stream = std::fopen(fileName.data(), fileMode.data());
         }
-    }
-
-    void openInStreamInTextAndSetBufSize(char *filename) {
-        if (0 == std::strncmp(filename, "stdin\0", 5)) {
-            file_in_stream = std::freopen(nullptr, "r", stdin);
-        } else {
-            file_in_stream = std::fopen(filename, "r");
-        }
-    }
-
-    void openOutStreamInBinaryAndSetBufSize(char *filename) {
-        if (0 == std::strncmp(filename, "stdout\0", 6)) {
-            file_out_stream = std::freopen(nullptr, "wb", stdout);
-        } else {
-            file_out_stream = std::fopen(filename, "wb+");
-        }
-    }
-
-
-    void openOutStreamInTextAndSetBufSize(char *filename) {
-        if (0 == std::strncmp(filename, "stdout\0", 6)) {
-            file_out_stream = std::freopen(nullptr, "w", stdout);
-        } else {
-            file_out_stream = std::fopen(filename, "w+");
-        }
+        return stream;
     }
 
     void setFileForProgram(std::span<char> fileName, FileModeCode op) {
-        assert(fileName.size()  < filename_max);
-        assert(fileName.end() == '\n');
+        assert(fileName.size()  < FILENAME_MAX);
+        assert(fileName.size() > 0);
+        assert(fileName[fileName.size() - 1] == '\n');
         FILE *fStream = nullptr;
-        fileName[std::strcspn(fileName, "\n")] = '\0';
+        fileName[std::strcspn(fileName.data(), "\n")] = '\0';
         switch (op) {
             case inputText:
-                openInStreamInTextAndSetBufSize(fileName);
-                fStream = file_in_stream;
+                fStream = openStreamInGivenMode(fileName, std::span<const char>(rt_mode), file_in_stream);
                 break;
             case inputBinary:
-                openInStreamInBinaryAndSetBufSize(fileName);
-                fStream = file_in_stream;
+                fStream = openStreamInGivenMode(fileName, std::span<const char>(rb_mode), file_in_stream);
                 break;
             case outputText:
-                openOutStreamInTextAndSetBufSize(fileName);
-                fStream = file_out_stream;
+                fStream = openStreamInGivenMode(fileName, std::span<const char>(wt_mode), file_out_stream);
                 break;
             case outputBinary:
-                openOutStreamInBinaryAndSetBufSize(fileName);
-                fStream = file_out_stream;
+                fStream = openStreamInGivenMode(fileName, std::span<const char>(wb_mode), file_out_stream);
                 break;
         }
-        checkopenfile(fStream);
-        setourbufsize(fStream);
+        checkOpenFile(fStream);
+        setOurBufSize(fStream);
+    }
+
+    std::size_t readBlockTillNL(const std::span<std::uint8_t> buf) {
+        assert(buf.size() == text_block_size);
+        std::size_t bytesRead = readBlock_1Byte(buf.subspan(0, buf.size() - max_20_digit));
+        if (eof_not_read && buf[bytesRead-1] != '\n') 
+            bytesRead += readTillNL(buf.subspan(buf.size() - max_20_digit, buf.size()));
+        remove_zero_factor(buf);
+        return bytesRead;
+    }
+
+    std::size_t setIndexLocations(const std::span<const std::uint8_t> buf, const std::span<std::size_t> indices) {
+        assert(indices.size() == text_block_max_numbers);
+        assert(buf.size() <= text_block_size);
+        std::size_t i = 0;
+        for (std::size_t b = 0; b < buf.size(); b++)
+            if (buf[b] == unsigned_line_break)
+                indices[i++] = b;
+        return i;
+    }
+
+    std::size_t getNextTextBlock(const std::span<std::uint8_t> buf, const std::span<std::size_t> indices) {
+        std::size_t bytesRead = readBlockTillNL(buf);
+        return setIndexLocations(buf.subspan(0, bytesRead), indices);
     }
 }
 
--- src/header/ProjectFileIO.hpp
+++ src/header/ProjectFileIO.hpp
@@ -2,7 +2,8 @@
 #define PROJECT_FILE_IO_HPP
 
 #include "IntegralTypeIncludes.hpp"
-#include "ContainersIncludes.hpp"
+#include "ContainerIncludes.hpp"
+#include "UtilityIncludes.hpp"
 #include <cinttypes> // for fixed width scan and print types
 
 // TODO fix typing am going to migrate to using fstream idk how good or bad it will be
@@ -18,26 +19,25 @@ namespace projectIO {
     void setOurBufSize(FILE *);
     void checkOpenFile(FILE *);
 
-    void writeBlock_1Byte(std::span<std::uint8_t>);
-    void writeBlock_8Byte(std::span<std::uint64_t>);
+    void writeBlock_1Byte(const std::span<const std::uint8_t>);
+    void writeBlock_8Byte(const std::span<const std::uint64_t>);
 
-    void writeBlockOfText(std::span<std::uint8_t>);
-    void flushBuffer(std::span<std::uint8_t>);
+    void writeBlockOfText(const std::span<std::uint8_t>);
+    void flushBuffer(const std::span<const std::uint8_t>);
 
-    std::size_t readTillNL(std::span<std::uint8_t>);
-    std::size_t readBlock_1Byte(std::span<std::uint8_t>);
+    std::size_t readTillNL(const std::span<std::uint8_t>);
+    std::size_t readBlock_1Byte(const std::span<std::uint8_t>);
 
-    std::size_t readBlockTillNL(std::span<std::uint8_t>);
-    std::size_t readBlockNBytes(std::span<std::uint8_t>);
+    std::size_t readBlockNBytes(const std::span<std::uint8_t>);
+    std::size_t readBlock_8Byte(const std::span<std::uint64_t>); 
 
-    std::size_t readBlock_8Byte(std::span<std::uint64_t>); 
-
-    void openInStreamInBinaryAndSetBufSize(char *); 
-    void openInStreamInTextAndSetBufSize(char *);
-    void openOutStreamInBinaryAndSetBufSize(char *); 
-    void openOutStreamInTextAndSetBufSize(char *);
+    FILE * openStreamInGivenMode(const std::span<const char>, const std::span<const char>, FILE *);
 
     void setFileForProgram(std::span<char>, FileModeCode);
+
+    std::size_t readBlockTillNL(const std::span<std::uint8_t>);
+    std::size_t setIndexLocations(const std::span<const std::uint8_t>, const std::span<std::size_t>);
+    std::size_t getNextTextBlock(const std::span<std::uint8_t>, const std::span<std::size_t>);
 }
 
 #endif
