5417943ea0f211448aae2b1dab59cc078b84435d
projectPythonator <danjkstraProtonAlt1@protonmail.com>
2025-01-17 07:16:48 -0500

    added in the options I am using for now At this time I will be restricting to one file we also have a few assumptions one assume all numbers given are primes assume all numbers given are consecutive primes

--- src/CliArgsParser.cpp
+++ src/CliArgsParser.cpp
@@ -7,30 +7,69 @@ namespace {
     constexpr std::size_t level_count = 12;
     constexpr std::size_t help_count = 2;
     constexpr std::size_t force_count = 2;
+    enum op_codes {
+        level_0,
+        level_1,
+        level_2,
+        level_3,
+        level_4,
+        level_5,
+        level_6,
+        job_op,
+        help_op,
+        test_op,
+        force_op,
+        stdout_op,
+        license_op,
+        version_op,
+        verbose_op,
+        decompress_op
+    }
 
-    constexpr std::string level_options[level_count] = {
-        "-0",
-        "-1",
-        "-2",
-        "-3",
-        "-4",
-        "-5",
-        "-6",
-        "-7",
-        "-8",
-        "-9",
-        "--best",
-        "--fast"
-    };
-    constexpr std::string help_options[help_count] = {
-        "-h",
-        "--help"
-    };
-    constexpr std::string force_options[force_count] = {
-        "-f",
-        "--force"
-    };
+    constexpr std::unordered_map<std::string, op_codes> cli_options = {
+        {"-0", level_0},
+        {"-1", level_1},
+        {"-2", level_2},
+        {"-3", level_3},
+        {"-4", level_4},
+        {"-5", level_5},
+        {"-6", level_6},
+        {"-7", level_6},
+        {"-8", level_6},
+        {"-9", level_6},
+        {"--fast", level_1},
+        {"--best", level_6},
+        
+        {"-h"    , help_op},
+        {"--help", help_op},
 
+        {"-f"       , force_op},
+        {"--force"  , force_op},
+
+        {"-j"       , job_op},
+        {"--jobs"   , job_op},
+
+        {"-t"       , test_op},
+        {"--test"   , test_op},
+
+        {"-c"           , stdout_op},
+        {"--stdout"     , stdout_op},
+        {"--to-stdout"  , stdout_op},
+
+        {"-L"           , license_op},
+        {"--license"    , license_op},
+
+        {"-V"           , version_op},
+        {"--version"    , version_op},
+
+        {"-d"           , decompress_op},
+        {"--decompress" , decompress_op},
+        {"--uncompress" , decompress_op},
+
+
+        {"-v"           , verbose_op},
+        {"--verbose"    , verbose_op}
+    };
 }
 
 namespace Parser {
