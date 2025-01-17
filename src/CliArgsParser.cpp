7938f54348b8d954a1143a23d3b4ca83f8128159
projectPythonator <danjkstraProtonAlt1@protonmail.com>
2025-01-16 19:11:54 -0500

    added in comment explaining what I am going for

--- src/CliArgsParser.cpp
+++ src/CliArgsParser.cpp
@@ -1,5 +1,12 @@
 #include "header/CliArgsParser.hpp"
 
+// I think I am going with
+// Prog [options] inputFile
+// if inputfile isn't selected the stdin option must be used
+namespace {
+
+}
+
 namespace Parser {
 
 
