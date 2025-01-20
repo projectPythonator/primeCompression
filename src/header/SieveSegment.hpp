85ad93ec7992bf9279608bae8b8c1b358081075a
projectPythonator <danjkstraProtonAlt1@protonmail.com>
2025-01-20 14:05:18 -0500

    added in api doc string for header file

--- src/header/SieveSegment.hpp
+++ src/header/SieveSegment.hpp
@@ -7,6 +7,14 @@
 #include "ProjectConstants.hpp"
 
 namespace SieveSegment {
+
+     /**
+     * @brief build the sieve mapping needing for higher level lookup
+     * call with params to get the sieve mapping needed to go from rb5 to rbX higher levels and viceversa
+     *
+     * @param segment   holds bit masks mapping it should confirm in size to primorial_5 / primorial_x
+     * @param level     the level to which we need to sieve 
+     */
     void sieveSegment(std::span<std::uint8_t>, std::size_t);
 }
 
