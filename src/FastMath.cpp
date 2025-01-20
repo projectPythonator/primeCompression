6df02f571caca68808394d7b4079bb97104cbf94
projectPythonator <danjkstraProtonAlt1@protonmail.com>
2025-01-20 13:36:33 -0500

    finalized how I want to do hashes we will just do a simple checksum    it will be 4 numbers hashes both ways using pi and prime number    4 values of meta data (1 is split in two since its 128 bit)

--- src/FastMath.cpp
+++ src/FastMath.cpp
@@ -1,87 +1,60 @@
 #include "header/FastMath.hpp"
+#include <cassert>
 
+
+// TODO add documentation
 namespace {
-    constexpr std::uint32_t golden_ratio_fp = 0x9e3779b9;
-    constexpr std::uint64_t pi_fp = 0x517cc1b727220a95ULL;
-    constexpr std::uint64_t random_prime_fp = 8579638605549648317ULL;
+    constexpr std::uint64_t pi_fp = 0x517cc1b727220a95ULL;      // pi inverse fixed point 
+    constexpr std::uint64_t prime_fp = 8579638605549648317ULL;  // random big prime number
     constexpr unsigned left_shift = 6u;
     constexpr unsigned right_shift = 2u;
 
-    std::uint32_t golden_hash(std::uint32_t lhs, std::uint32_t rhs) {
-        return lhs ^ (rhs + golden_ratio_fp + (lhs << left_shift) + (lhs >> right_shift));
+    // following taken from SO: https://stackoverflow.com/a/57556517 edited 
+    constexpr std::uint64_t p_value = 0x5555555555555555ULL;    // pattern of alternating 0 and 1
+    constexpr std::uint64_t c_value = 17316035218449499591ULL;  // random uneven integer constant
+    constexpr unsigned xor_shift_amount = 32;                   // wsa changed from int to unsigned
+                                                                // as per c++ core guidelines
+
+    std::uint64_t xorShift(const std::uint64_t& n, unsigned i){
+        return n^(n>>i);
     }
+
+    std::uint64_t xorHash(const std::uint64_t& n){
+        return c_value * xorShift(p_value * xorShift(n, xor_shift_amount), xor_shift_amount);
+    }
+    // taken section over
     
-    std::uint64_t pi_hash(std::uint64_t lhs, std::uint64_t rhs) {
-        return lhs ^ (rhs + pi_fp + (lhs << left_shift) + (lhs >> right_shift));
+    // boost style hash combine seen ^= hash(value) + const + (seed << 6) + (seed >> 2)
+    // edited to conform to c++ core guidelines
+    std::uint64_t boostHashCombine(std::uint64_t fixedPoint, std::uint64_t lhs, std::uint64_t rhs) {
+        return lhs ^ (xorHash(rhs) + fixedPoint + (lhs << left_shift) + (lhs >> right_shift));
     }
+    // boost code over
 
-    std::uint64_t big_prime_hash(std::uint64_t lhs, std::uint64_t rhs) {
-        return lhs ^ (rhs + random_prime_fp + (lhs << left_shift) + (lhs >> right_shift));
+    // function here to just provide a way to get combined hashes 
+    void hashBatch(std::span<const std::uint64_t> values, std::uint64_t &piHash, std::uint64_t &primeHash) {
+        std::uint64_t piResult = values[0];
+        std::uint64_t primeResult = values[0];
+        for (const std::uint64_t &value: values) {
+            piResult = boostHashCombine(pi_fp, piResult, value);
+            primeResult = boostHashCombine(prime_fp, primeResult, value);
+        }
+        piHash = piResult;
+        primeHash = primeResult;
     }
-
-    void golden_batch(std::span<std::uint64_t> hashes, std::uint64_t value, std::size_t e) {
-        std::uint32_t a = (uint32_t)value;
-        std::uint32_t b = (uint32_t)std::hash<std::uint64_t>{}(value);
-        hashes[0] = golden_hash((uint32_t) hashes[0], a);
-        hashes[1] = golden_hash((uint32_t) hashes[1], b);
-
-        hashes[2] = golden_hash((uint32_t) hashes[2], a);
-        hashes[2] = golden_hash((uint32_t) hashes[2], b);
-        
-        hashes[e] = golden_hash((uint32_t) hashes[e], a);
-        hashes[e] = golden_hash((uint32_t) hashes[e], b);
-    }
-
-    void pi_batch(std::span<std::uint64_t> hashes, std::uint64_t value, std::size_t e) {
-        std::uint64_t a = value;
-        std::uint64_t b = std::hash<std::uint64_t>{}(value);
-
-        hashes[0] = pi_hash(hashes[0], a);
-        hashes[1] = pi_hash(hashes[1], b);
-
-        hashes[2] = pi_hash(hashes[2], a);
-        hashes[2] = pi_hash(hashes[2], b);
-
-        hashes[e] = pi_hash(hashes[e], a);
-        hashes[e] = pi_hash(hashes[e], b);
-    }
-
-    void prime_batch(std::span<std::uint64_t> hashes, std::uint64_t value, std::size_t e) {
-        std::uint64_t a = value;
-        std::uint64_t b = std::hash<std::uint64_t>{}(value);
-
-        hashes[0] = big_prime_hash(hashes[0], a);
-        hashes[1] = big_prime_hash(hashes[1], b);
-
-        hashes[2] = big_prime_hash(hashes[2], a);
-        hashes[2] = big_prime_hash(hashes[2], b);
-
-        hashes[e] = big_prime_hash(hashes[e], a);
-        hashes[e] = big_prime_hash(hashes[e], b);
-    }
-
 }
 
 namespace FastMath {
+   
     void generate_hashes(const std::span<const std::uint64_t> metaData, std::span<std::uint64_t> hashes) {
-        for (std::uint64_t value: metaData) {
-            hashes[4] = big_prime_hash(hashes[4], value);
-            hashes[5] = big_prime_hash(hashes[5], std::hash<std::uint64_t>{}(value));
-
-            hashes[0] = golden_hash((uint32_t) hashes[0], (uint32_t)value);
-            hashes[1] = golden_hash((uint32_t) hashes[1], (uint32_t)std::hash<std::uint64_t>{}(value));
-            hashes[2] = pi_hash(hashes[2], value);
-            hashes[3] = pi_hash(hashes[3], std::hash<std::uint64_t>{}(value));
-            hashes[4] = big_prime_hash(hashes[4], value);
-            hashes[5] = big_prime_hash(hashes[5], std::hash<std::uint64_t>{}(value));
-
-            hashes[9] = golden_hash((uint32_t) hashes[9], (uint32_t)value);
-            hashes[9] = golden_hash((uint32_t) hashes[9], (uint32_t)std::hash<std::uint64_t>{}(value));
-            hashes[9] = pi_hash(hashes[9], value);
-            hashes[9] = pi_hash(hashes[9], std::hash<std::uint64_t>{}(value));
-            hashes[9] = big_prime_hash(hashes[9], value);
-            hashes[9] = big_prime_hash(hashes[9], std::hash<std::uint64_t>{}(value));
-
+        assert(metaData.size() == 5u);
+        std::array<std::uint64_t, 5u> metaFor{0};
+        std::array<std::uint64_t, 5u> metaRev{0};
+        for (std::size_t i = 0; i < metaData.size(); i++) {
+            metaFor[i] = metaData[i];
+            metaRev[(metaData.size()-i)-1] = metaData[i];
         }
+        hashBatch(std::span<const std::uint64_t>(metaFor.begin(), metaFor.size()), hashes[0], hashes[1]);
+        hashBatch(std::span<const std::uint64_t>(metaRev.begin(), metaRev.size()), hashes[2], hashes[3]);
     }
 }
