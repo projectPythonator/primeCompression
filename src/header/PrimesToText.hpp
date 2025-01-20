945f04f21a6e64671f4c3b69a51035c1be402be7
projectPythonator <danjkstraProtonAlt1@protonmail.com>
2025-01-20 14:35:34 -0500

    added in the header file doc strings

--- src/header/PrimesToText.hpp
+++ src/header/PrimesToText.hpp
@@ -7,10 +7,25 @@
 
 
 namespace EndPointConversions {
+    /**
+     * @brief convert number n to string and store it in the span
+     *
+     * @assumption_1    no prime is 0
+     */
     void addNextNumberBase10_XXd(std::span<std::uint8_t>, std::uint64_t);
-
     void addNextNumberBase10(std::span<std::uint8_t>, std::uint64_t);
 
+    /**
+     * @brief convert list of numbers into a buffer
+     * call with the params and you will recieve a buffer of text in numbers
+     *
+     * @param primes    a list of primes to convert
+     * @param numbers   buffer to hold the output
+     * @return  end index of the buffer
+     *
+     * @assumption_1    numbers is big enough to hold primes
+     * @assumption_1    no prime is 0
+     */
     std::size_t convertPrimesBlock(std::span<const std::uint64_t>, std::span<std::uint8_t>);
 }
 #endif
