cfdcc497391ee256f447e34316ceb569ad9a8cea
projectPythonator <danjkstraProtonAlt1@protonmail.com>
2025-01-20 23:09:58 -0500

    Added in doc strings for the header of io
    I think I might have found how I want to do the doc strings in headers
    if possible

--- src/header/ProjectFileIO.hpp
+++ src/header/ProjectFileIO.hpp
@@ -16,23 +16,87 @@ namespace ProjectIO {
         outputBinary
     };
 
-
+    /**
+     * @brief Call when you need to print block of text
+     *
+     * @param buf   span of bytes to print
+     * @assumption  buffer isn't normalized with the '0' char
+     */
     void writeBlockOfText(const std::span<std::uint8_t>);
+
+    /**
+     * @brief Call when you need to print block of binary 8 byte numbers
+     *
+     * @param buf   span of numbers to print
+     */
     void writeBlockOfBinary(const std::span<std::uint64_t>);
 
+    /**
+     * @brief Call when you need to truncate a byte buffer.
+     *
+     * @param buf   span of bytes to print
+     * @assumption  buffer isn't size 0. (checked)
+     */
     void flushBuffer(const std::span<const std::uint8_t>);
 
-    std::size_t readTillNL(const std::span<std::uint8_t>);
+    /**
+     * @brief Call when you want to read a block of bytes.
+     *
+     * @param buf   span that will hold the bytes read in.
+     * @return      count of bytes read.
+     */
     std::size_t readBlock_1Byte(const std::span<std::uint8_t>);
 
+    /**
+     * @brief Call when you want to read a block of numbers in 8 byte format.
+     *
+     * @param buf   span that will hold the numbers in it.
+     * @return      count of numbers read.
+     */
+    std::size_t readBlock_8Byte(const std::span<std::uint64_t>);
+
+    /**
+     * @brief Call when you want to read a set number of bytes from stream.
+     *
+     * @param buf   span that will hold the bytes.
+     * @return      count of bytes read.
+     */
     std::size_t readBlockNBytes(const std::span<std::uint8_t>);
-    std::size_t readBlock_8Byte(const std::span<std::uint64_t>); 
 
-    void setFileForProgram(std::span<char>, FileModeCode);
+    /**
+     * @brief Call only when you want to read one number from stream.
+     *
+     * @param buf   span that will hold the number.
+     * @return      count of bytes read.
+     *
+     * @assumption  buffer size <= 20 (checked).
+     */
+    std::size_t readTillNL(const std::span<std::uint8_t>);
 
+    /**
+     * @brief Call only when you want to read numbers up till a newline
+     *
+     * @param buf   span that will hold the numbers in a buffer.
+     * @return      count of bytes read.
+     */
     std::size_t readBlockTillNL(const std::span<std::uint8_t>);
-    std::size_t setIndexLocations(const std::span<const std::uint8_t>, const std::span<std::size_t>);
+
+    /**
+     * @brief Call when you want both a text block and mapping of newlines.
+     *
+     * @param buf       span that will hold the block of characters.
+     * @param indices   span that holds positions of newlines.
+     * @return          count of bytes read.
+     */
     std::size_t getNextTextBlock(const std::span<std::uint8_t>, const std::span<std::size_t>);
+
+    /**
+     * @brief Call when you want to open a file for the program.
+     *
+     * @param fileName  span holding name of file. (stdin, stdout for those files)
+     * @param op        enum for mapping of an operation code (found in header)
+     */
+    void setFileForProgram(std::span<char> fileName, FileModeCode op);
 }
 
 #endif
