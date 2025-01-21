fe7f64f9e8824632a6d0b999d7cc140c11a8b9d2
projectPythonator <danjkstraProtonAlt1@protonmail.com>
2025-01-21 07:25:41 -0500

    added in better docs and new header docs Preformance bug found in source file changed.     in compressblock: I assign per call I should change it to one assign else where then     just take a span.     in decompress: I reserve and push_back I wonder if thre is a faster     way to count bits on.

--- src/ChangeBetweenPrimesAndBlocks.cpp
+++ src/ChangeBetweenPrimesAndBlocks.cpp
@@ -18,8 +18,8 @@ namespace EndpointConversion {
      * @brief compress primes
      * Given list of existing primes compress them.
      *
-     * @param primes_in A list of prines in 8 byte form.
-     * @param primes_out A block of bytes representing the compressed primes.
+     * @param primesIn  Span of primes in 8 byte form.
+     * @param primesOut List of bytes representing the compressed primes.
      *
      * @optimization_1 change vector type to array and have static size.
      * @optimization_2 change inner function calls to / and % right now from my ad hoc profiling
@@ -27,30 +27,30 @@ namespace EndpointConversion {
      *  and sometimes was a bit slower. Was ad hoc because not tested on this program just raw
      *  speed. The calls were consistent though so will use them until this is a bottle neck.
      */
-    void compressBlock(const std::span<std::uint64_t> primes_in, std::vector<std::uint8_t> primes_out) {
-        std::size_t blockSize = FastMath::fastDivideBy30(primes_in.back()) - FastMath::fastDivideBy30(primes_in.front());
-        primes_out.assign(blockSize, 0);
-        for(const auto& prime: primes_in)
-            primes_out[FastMath::fastDivideBy30(prime) % blockSize] |= ProjectConstants::offset_to_mask[FastMath::fastModBy30(prime)];
+    void compressBlock(const std::span<std::uint64_t> primesIn, std::vector<std::uint8_t> primesOut) {
+        std::size_t blockSize = FastMath::fastDivideBy30(primesIn.back()) - FastMath::fastDivideBy30(primesIn.front());
+        primesOut.assign(blockSize, 0); // this might need to be moved along with line above it.
+        for(const auto& prime: primesIn)
+            primesOut[FastMath::fastDivideBy30(prime) % blockSize] |= ProjectConstants::offset_to_mask[FastMath::fastModBy30(prime)];
     }
 
     /**
      * @brief extract primes
      * Extract compressed primes stored in bytes and convert them to a list of 8 byte numbers.
      *
-     * @param primes_in   A block of compressed primes
-     * @param primes_out  A container to store extracted primes in
-     * @param offset      The starting point that our compressed primes start at
+     * @param primesIn  Span of bytes representing the compressed primes.
+     * @param primesOut List of 8 byte numbers to store the primes in.
+     * @param offset    The starting point that our compressed primes start at.
      *
      * @optimization_1 can replace push_back with indexing if you create with enough space or calc the popcount.
      * @optimization_2 run sections in parallel maybe that work on different sections of a block.
      */
-    void decompressBlock(const std::span<std::uint8_t> primes_in, std::vector<std::uint64_t> primes_out, std::uint64_t offset) {
-        primes_out.reserve(8 * primes_in.size());
-        for (std::size_t i = 0; i < primes_in.size(); i++)
-            if (primes_in[i]) // blocks with primes require bits to be on 
-                for (std::uint8_t mask = primes_in[i]; mask; mask ^= (-mask & mask)) // turn off lsb each iteration
-                    primes_out.push_back(extractNextPrime(offset + i, mask));
-        primes_out.shrink_to_fit();
+    void decompressBlock(const std::span<std::uint8_t> primesIn, std::vector<std::uint64_t> primesOut, std::uint64_t offset) {
+        primesOut.reserve(8 * primesIn.size());
+        for (std::size_t i = 0; i < primesIn.size(); i++)
+            if (primesIn[i]) // blocks with primes require bits to be on 
+                for (std::uint8_t mask = primesIn[i]; mask; mask ^= (-mask & mask)) // turn off lsb each iteration
+                    primesOut.push_back(extractNextPrime(offset + i, mask));
+        primesOut.shrink_to_fit();
     }
 }
--- src/header/ChangeBetweenPrimesAndBlocks.hpp
+++ src/header/ChangeBetweenPrimesAndBlocks.hpp
@@ -7,8 +7,31 @@
 #include "ProjectConstants.hpp"
 
 namespace EndpointConversion {
+    void decompressBlock(const std::span<std::uint8_t>, std::vector<std::uint64_t>, std::uint64_t);
+    /**
+     * @brief call to extra prime number from compressed byte.
+     *
+     * @param block Is the offset which our prime is at.
+     * @param mask  Holds the next prime in byte form.
+     * @return 8 byte prime number
+     */
     std::uint64_t extractNextPrime(std::uint64_t, std::uint8_t);
+
+    /**
+     * @brief Call to compress a block of prime numbers into bytes.
+     *
+     * @param primesIn  Span of primes in 8 byte form.
+     * @param primesOut List of bytes representing the compressed primes.
+     */
     void compressBlock(const std::span<std::uint64_t>, std::vector<std::uint8_t>);
+
+    /**
+     * @brief Call to extract a list of primes from a block of bytes.
+     *
+     * @param primesIn  Span of bytes representing the compressed primes.
+     * @param primesOut List of 8 byte numbers to store the primes in.
+     * @param offset    The starting point that our compressed primes start at.
+     */
     void decompressBlock(const std::span<std::uint8_t>, std::vector<std::uint64_t>, std::uint64_t);
 }
 
