55cc276633266b4c1011d7df2345362e51ecb361
projectPythonator <danjkstraProtonAlt1@protonmail.com>
2025-01-22 08:15:48 -0500

    added includes to make sure it compiles nicely

--- src/PrimeCompression.cpp
+++ src/PrimeCompression.cpp
@@ -1,8 +1,15 @@
 #include "header/CliArgsParser.hpp"
+#include "header/BuildRunTimeData.hpp"
+#include "header/ChangeBetweenPrimesAndBlocks.hpp"
+#include "header/ChangeLevelHigher.hpp"
+#include "header/ChangeLevelLower.hpp"
+#include "header/FastMath.hpp"
+#include "header/ProjectFileIO.hpp"
+#include "header/PrimesToText.hpp"
+#include "header/TextToPrimes.hpp"
+#include "header/SieveSegment.hpp"
 
 
-//int main(int argc, char *argv[]) {
 int main() {
-
     return 0;
 }
