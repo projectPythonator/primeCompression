7c782ff4d3a05055dc537cd5deaefa73275046a6
projectPythonator <danjkstraProtonAlt1@protonmail.com>
2025-01-22 08:09:49 -0500

    added in the function doc strings for the header

--- src/header/CliArgsParser.hpp
+++ src/header/CliArgsParser.hpp
@@ -7,9 +7,23 @@
 #include "UtilityIncludes.hpp"
 
 namespace Parser {
+    /**
+     * @brief   Call to handle strings that don't fall under the option choices.
+     *
+     * @param token         string that holds the current cli arguement.
+     * @param fileName      string we use to store if it is a file name
+     * @param threadCount   number we use to store token if it is a number.
+     */
     void handleNotOption(const std::span<const char>, std::span<char>, std::size_t &);
 
-    //void handleArg(const std::span<const char>, std::span<char>, std::span<std::size_t>);
+    /**
+     * @brief   Call to parse data from the cli args.
+     *
+     * @param argc      Standard c main cli args, the number.
+     * @param argv      Standard c main cli args, the strings.
+     * @param fileName  Will hold any file names we get.
+     * @param values    Will hold any number based arguements we get.
+     */
     void handleArgs(int, char *, std::span<char>, std::span<std::size_t>);
 }
 
