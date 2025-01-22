ccd436625d003091155b4245f41ded18ffa4e873
projectPythonator <danjkstraProtonAlt1@protonmail.com>
2025-01-22 07:56:48 -0500

    added in func docs for header forgot last commit

--- src/header/BuildRunTimeData.hpp
+++ src/header/BuildRunTimeData.hpp
@@ -12,6 +12,15 @@
 
 
 namespace InitData {
+
+    /**
+     * @brief Call to set up program based on cli arguments given.
+     *
+     * @param inName    String that holds the in file name [stdin default].
+     * @param outName   String that holds the out file name [stdout default].
+     * @param level     Number indicating the desired compression.
+     * @param option    Number holding the io option. (need to create an enum).
+     */
     void initProgramData(const std::span<const char>, const std::span<const char>, std::size_t, std::size_t);
 }
 
