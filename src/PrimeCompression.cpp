518065af49756cbf395e57e3f850947df5310d6b
projectPythonator <danjkstraProtonAlt1@protonmail.com>
2025-01-25 18:29:56 -0500

    added in some testing to make sure things work

--- src/PrimeCompression.cpp
+++ src/PrimeCompression.cpp
@@ -8,8 +8,23 @@
 #include "header/PrimesToText.hpp"
 #include "header/TextToPrimes.hpp"
 #include "header/SieveSegment.hpp"
+#include <iostream>
 
 
 int main() {
+    std::vector<std::uint64_t> test1(10, 0);
+    std::vector<std::uint8_t> test2(100, 0);
+    for (std::uint64_t i = 0; i < test1.size(); i++)
+        test1[i] = i+100;
+    std::cout << "here" << std::endl;
+    std::size_t amt = EndPointConversions::convertPrimesBlock(std::span<uint64_t>(test1), std::span<std::uint8_t>(test2));
+    std::cout << "here2" << std::endl;
+    for (const std::uint64_t el: test1)
+        std::cout << el << std::endl;
+    std::cout << "here3" << std::endl;
+    std::cout << amt << std::endl;
+    for (const std::uint8_t el: std::span(test2.begin(), amt))
+        std::cout << (char)(el+'0');
+    std::cout << "hello world";
     return 0;
 }
